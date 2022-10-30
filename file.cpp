180ec1495a9a63990ed28e006282f7d1a15a60df
haider-9415 <116593197+haider-9415@users.noreply.github.com>
2022-10-30 14:50:36 +0530

    Update file.cpp

--- file.cpp
+++ file.cpp
@@ -1,27 +1,27 @@
 #include <iostream>
 using namespace std;
 int main(){
-    // // it is to print n!
-    // int y,z=1;
-    // cin>>y;
-    // for (int i = y; i >= 1; i--)
-    // {
-    //     z*=i;
-    // }
-    // cout<<z;
+    // it is to print n!
+    int y,z=1;
+    cin>>y;
+    for (int i = y; i >= 1; i--)
+    {
+        z*=i;
+    }
+    cout<<z;
 
-    // // it is to print a to the power b
-    // int j=1,a,b;
-    // cin>>a;
-    // cin>>b;
-    // for (int i = 1; i <= b; i++)
-    // {
-    //     j*=a;    // in this a multiply by itself b times
-    // }
-    // cout<<j;
+    // it is to print a to the power b
+    int j=1,a,b;
+    cin>>a;
+    cin>>b;
+    for (int i = 1; i <= b; i++)
+    {
+        j*=a;    // in this a multiply by itself b times
+    }
+    cout<<j;
 
     
-    // // To find first n terms of the sequence " n(n+2)"
+    // To find first n terms of the sequence " n(n+2)"
     int n,i=1;
     cout<<"Enter no. of terms \n";
     cin>>n;
@@ -34,7 +34,7 @@ int main(){
         i++;
     }
     cout<<endl;
-    // // sum of the series of the sequence " n(n+2) "
+    // sum of the series of the sequence " n(n+2) "
     int ans2=0;
     for (int i = 1; i <= n; n--)
     {
@@ -44,28 +44,28 @@ int main(){
     cout<<"Sum of the series; "<<ans2;
 
 
-    // // To find first n terms of sequence " n(n-2)/n+3 .But it gives only integer values"
-    // int n1,i1=1;
-    // cin>>n1;
-    // cout<<"Sequence: ";
-    // while (i1<=n1)
-    // {
+    // To find first n terms of sequence " n(n-2)/n+3 .But it gives only integer values"
+    int n1,i1=1;
+    cin>>n1;
+    cout<<"Sequence: ";
+    while (i1<=n1)
+    {
                 
-    //     float ans=(i1*(i1-2)/(i1+3));
-    //     cout<<ans<<",";
-    //     i1++;
-    // }
-    // cout<<endl;
+        float ans=(i1*(i1-2)/(i1+3));
+        cout<<ans<<",";
+        i1++;
+    }
+    cout<<endl;
 
-    // // To find sum of the series " n(n-2)/n+3 but in integer value"
-    // int i2=1,ans1=0;
-    // // cin>>n1;
-    // while (i2<=n1)
-    // {
+    // To find sum of the series " n(n-2)/n+3 but in integer value"
+    int i2=1,ans1=0;
+    // cin>>n1;
+    while (i2<=n1)
+    {
                 
-    //     ans1+=(i2*(i2-2)/(i2+3));
-    //     i2++;
-    // }
-    // cout<<"Sum of the series: "<<ans1;
+        ans1+=(i2*(i2-2)/(i2+3));
+        i2++;
+    }
+    cout<<"Sum of the series: "<<ans1;
 
 }
